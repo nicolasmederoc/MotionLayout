e5cd60548e6ee3e504e850cf42cd7876a6604a5a
Nicolás Medero C <nicolas.medero@wolox.com.ar>
2019-09-20 17:41:18 -0300

    Adding Motion Layout

--- app/src/main/java/ar/com/wolox/android/example/ui/example/ExampleFragment.kt
+++ app/src/main/java/ar/com/wolox/android/example/ui/example/ExampleFragment.kt
@@ -3,22 +3,16 @@ package ar.com.wolox.android.example.ui.example
 import android.content.Intent
 import ar.com.wolox.android.R
 import ar.com.wolox.android.example.ui.viewpager.ViewPagerActivity
-import ar.com.wolox.android.example.utils.onClickListener
-import ar.com.wolox.android.example.utils.onTextChanged
 import ar.com.wolox.wolmo.core.fragment.WolmoFragment
-import kotlinx.android.synthetic.main.fragment_example.*
 
 class ExampleFragment : WolmoFragment<ExamplePresenter>(), IExampleView {
 
     override fun layout(): Int = R.layout.fragment_example
 
     override fun init() {
-        vLoginButton.isEnabled = false
     }
 
     override fun setListeners() {
-        vUsernameInput.onTextChanged { vLoginButton.isEnabled = it.isNotBlank() }
-        vLoginButton.onClickListener { presenter.onLoginButtonClicked(vUsernameInput.text.toString()) }
     }
 
     override fun goToViewPager() {
