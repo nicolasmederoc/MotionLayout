798d85f13013c3ec5112cfe3664a4b62d560b63e
Nicolás Medero C <nicolas.medero@wolox.com.ar>
2019-09-25 18:34:26 -0300

    Add examples

--- app/src/main/java/ar/com/wolox/android/example/ui/example/ExampleFragment.kt
+++ app/src/main/java/ar/com/wolox/android/example/ui/example/ExampleFragment.kt
@@ -1,18 +1,55 @@
 package ar.com.wolox.android.example.ui.example
 
 import android.content.Intent
+import android.os.Handler
+import androidx.viewpager.widget.ViewPager
 import ar.com.wolox.android.R
 import ar.com.wolox.android.example.ui.viewpager.ViewPagerActivity
+import ar.com.wolox.android.example.ui.viewpager.random.RandomFragment
+import ar.com.wolox.wolmo.core.adapter.viewpager.SimpleFragmentPagerAdapter
 import ar.com.wolox.wolmo.core.fragment.WolmoFragment
+import kotlinx.android.synthetic.main.fragment_viewpager_with_motionlayout.*
+import kotlinx.android.synthetic.main.motionlayout_for_viewpager.*
+import javax.inject.Inject
 
 class ExampleFragment : WolmoFragment<ExamplePresenter>(), IExampleView {
+
+    @Inject lateinit var randomFragment1: RandomFragment
+    @Inject lateinit var randomFragment2: RandomFragment
+    private lateinit var fragmentPagerAdapter: SimpleFragmentPagerAdapter
+    private var numberOfPages: Int = 0
 
     override fun layout(): Int = R.layout.fragment_example
 
     override fun init() {
+        /*fragmentPagerAdapter = SimpleFragmentPagerAdapter(childFragmentManager)
+        fragmentPagerAdapter.addFragments(
+                Pair(randomFragment1, "Page 1"),
+                Pair(randomFragment2, "Page 2")
+        )
+        vViewPagerMotion.adapter = fragmentPagerAdapter
+        vHomeTabsMotion.setupWithViewPager(vViewPagerMotion)
+        numberOfPages = 2
+        setUpViewPagerMotionListener()*/
     }
 
     override fun setListeners() {
+    }
+
+    private fun setUpViewPagerMotionListener() {
+        vViewPagerMotion.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
+            override fun onPageScrollStateChanged(state: Int) {
+            }
+
+            override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {
+                Handler().postDelayed({
+                    /* change motion layout progress according to the viewPager position */
+                    vMotionLayout.progress = (position + positionOffset) / (numberOfPages - 1)
+                }, 100)
+            }
+
+            override fun onPageSelected(position: Int) {}
+        })
     }
 
     override fun goToViewPager() {
